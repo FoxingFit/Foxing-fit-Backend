89c5a1938188e46363856f7ad64b0e8ecd2d1b8e
numanijaz119 <minibhai009@gmail.com>
2026-02-05 20:52:16 +0500

    database syntex fixed in backend

--- backend/settings.py
+++ backend/settings.py
@@ -82,9 +82,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
